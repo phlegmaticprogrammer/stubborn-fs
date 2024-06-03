6ebb12c658e6817e6006e101f9c518df447c0765
Steven Obua <steven@recursivemind.com>
2024-06-03 12:28:29 +0100

    prepare for npm repo

--- src/constants.ts
+++ src/constants.ts
@@ -9,7 +9,7 @@ const IS_USER_ROOT = process.getuid ? !process.getuid () : false;
 
 const LIMIT_FILES_DESCRIPTORS = 10_000; //TODO: Fetch the real limit from the filesystem, somehow
 
-const NOOP = () => undefined;
+const NOOP : () => undefined = () => undefined;
 
 /* EXPORT */
 
